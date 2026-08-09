a0db8d27c9de0391a38e27d4cf8f885a9ae9e1da
swapnilkumar82 <swapnilkumar3.142857@gmail.com>
2026-08-09 21:10:56 +0530

    Add files via upload

---
+++ boolean.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+int main(){
+	_Bool HiRelyIs=0;
+	printf("your reply is :%i(0=no and 1=yes)\n",HiRelyIs);
+	return 0;
+}*/
+			
+
+
+			// or like this 
+
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+	bool hi_reply_is= false;
+	printf("your reply is :%i(0=no and 1=yes)\n",hi_reply_is);
+	return 0;
+}	
