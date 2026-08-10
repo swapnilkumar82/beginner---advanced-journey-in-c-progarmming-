d49c8fd2b0cc034cd4797b17545d4cb786ef117f
swapnilkumar82 <swapnilkumar3.142857@gmail.com>
2026-08-10 09:59:27 +0530

    Add files via upload

---
+++ incrementdecrementAssignment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int books=100;
+	printf("%i\n",books);
+	 books +=100;//200
+	printf("%i\n",books);
+	 books -=100;//100
+	printf("%i\n",books);
+	 books *=10;//1000
+	printf("%i\n",books);
+	 books /=10;//100
+	printf("%i\n",books);
+	 books %=10;//0
+	printf("%i\n",books);
+	return 0;
+}
