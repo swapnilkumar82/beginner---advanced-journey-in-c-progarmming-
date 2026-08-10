6ab30aa7da9e714b30c344f1e30f1e43898d3883
swapnilkumar82 <swapnilkumar3.142857@gmail.com>
2026-08-10 09:47:35 +0530

    Add files via upload

---
+++ incrementdecrement.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main(){
+	 int books=100;
+	 int output= ++books;
+         int output1=--books;
+         printf("%i\n",output);
+         printf("%i\n",output1);
+         return 0;
+}
+
