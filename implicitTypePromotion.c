7646fc2003a083032df17e992a4efe7649fec2cf
swapnilkumar82 <swapnilkumar3.142857@gmail.com>
2026-08-11 21:42:27 +0530

    Add files via upload

---
+++ implicitTypePromotion.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+
+	float x=99.0;
+	printf("x=%f\n",x);// but the result is 99.000000 but it a double so, in c both float and double are store in same but before storing it is a float 
+
+	return 0;
+}
