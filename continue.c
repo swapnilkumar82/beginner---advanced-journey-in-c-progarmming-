bd35d842751f2408b2a1c3995f810a3ea211a236
swapnil kumar <swapnilkumar3.142857@gmail.com>
2026-08-17 12:18:27 +0530

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int i=0;
+while(i<10)
+{
+if(i==7)
+{
+printf("Thala for a reason\n");
+    i++;
+ // continue;
+}
+
+printf("%d\n",i);
+i++;
+
+}
+
+
+
+    return 0;
+}
